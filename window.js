6f9289f0469c497f139cd4509d788a9debcdb15a
12cosqun12-boop <12cosqun12@gmail.com>
2026-04-30 19:06:57 +0400

    Update window.js

--- window.js
+++ window.js
@@ -1,1 +1,10 @@
+const { exec } = require('child_process');
 
+function openWindow(title, message) {
+    // Linux üzerinde sistem penceresi açar
+    exec(`zenity --info --title="${title}" --text="${message}" --width=300`, (err) => {
+        if (err) console.error("Window Error:", err);
+    });
+}
+
+module.exports = { openWindow };
